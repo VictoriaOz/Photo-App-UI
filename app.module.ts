f64206e4d81d8f5117200404bb2db88fbb0abe1e
Victoria Ozment <59891030+VictoriaOz@users.noreply.github.com>
2021-08-12 18:06:05 -0500

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
+import { PhotoGalleryImageComponent } from './photo-gallery-image/photo-gallery-image.component';
+
+@NgModule({
+  declarations: [AppComponent, PhotoGalleryComponent, PhotoGalleryImageComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
